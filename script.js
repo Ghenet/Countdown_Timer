2a0446d98c0451c97b69e5ad3ce2130fcc59feb9
Ghenet Andemichael <38365838+Ghenet@users.noreply.github.com>
2022-03-07 16:04:47 -0800

    adds a time format function

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
 const daysEl = document.getElementById("days");
 const hoursEl = document.getElementById("hours");
-const minsEl = document.getElementById("minutes");
+const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
 const newYear = "1 Jan 2023";
 
-function countdown {
-    const newYearDate = new Date(newYears);
+function countdown() {
+    const newYearDate = new Date(newYear);
     const currentDate = new Date();
 
-    const totalSeconds = (newYearsDate - currentDate)/ 1000;
+    const totalSeconds = (newYearDate - currentDate)/ 1000;
 
     const days = Math.floor(totalSeconds / 3600 / 24);
     const hours = Math.floor(totalSeconds/ 3600) % 24;
-    const mins = Math.floor(totalseconds / 60) % 60;
+    const mins = Math.floor(totalSeconds / 60) % 60;
     const seconds = Math.floor(totalSeconds)% 60;
     
 
     daysEl.innerHTML = days;
     hoursEl.innerHTML = hours;
-    minsEl.innerHTML = mins;
-    secondsEl.innerHTML = seconds;
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+}
+
+//adds 0 before the number if it's single digit time
+function formatTime(time){
+    return time < 10 ? `0${time}` : time;
 }
 
 // initial call
